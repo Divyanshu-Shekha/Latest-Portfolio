913e208a04a256dbaaf53a05be68f05b8afe11fb
Divyanshu Shekhar <114979302+Divyanshu-Shekha@users.noreply.github.com>
2024-09-22 21:14:08 +0530

    new commit

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,38 +1,61 @@
 import React from "react";
 import "../style/about.css";
-import my_image from "../Assets/image.png"
+import my_image from "../Assets/image.png";
 
 const About = () => {
   return (
     <>
       <div className="AboutContainer">
         <div className="ImageContainer">
-          <img src={my_image} alt="developer"/>
+          <img src={my_image} alt="developer" />
         </div>
         <div className="Detail_Container">
-          <h1>MY BIOGRAPHY</h1>
+          <h1>Biography of Divyanshu Shekhar</h1>
           <div className="blue_line"></div>
           <p>
-            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quas
-            nesciunt quia obcaecati. Cumque deserunt sequi pariatur voluptates
-            nesciunt exercitationem, enim quidem repellat impedit? Molestias
-            amet ducimus debitis pariatur, perspiciatis quae!
+            Divyanshu Shekhar, born on September 5, 2000, in Saharsa, Bihar, is
+            an aspiring tech enthusiast currently based in Delhi. With a passion
+            for technology and a drive for perfection, he is forging his career
+            as a full-stack developer, currently interning at Ducat. His journey
+            in the tech world began early, and over the years, he has developed
+            expertise across both backend and frontend development, coupled with
+            strong leadership skills.
           </p>
           <p>
-            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quas
-            nesciunt quia obcaecati. Cumque deserunt sequi pariatur voluptates
-            nesciunt exercitationem, enim quidem repellat impedit? Molestias
-            amet ducimus debitis pariatur, perspiciatis quae!
+            Divyanshu pursued his B.Tech from the Panipat Institute of
+            Engineering and Technology, following a diploma from CDL Govt.
+            Polytechnic. His educational journey is marked by continuous growth,
+            as reflected in his three significant certifications: a PHP
+            internship, a Python internship, and a specialization in MERN stack
+            development from PIET. He thrives in competitive environments,
+            demonstrated by his achievement in a hackathon where he was awarded
+            for maintaining his position.
+          </p>
+          <p>
+            Dedicated to mastering new technologies, Divyanshu is always eager
+            to expand his skill set. He believes in delivering work with
+            precision, following the principle that only perfection is
+            acceptable. As he looks to the future, his goal is clear: to secure
+            a leadership role where he can guide a team and shape innovative
+            tech solutions.
+          </p>
+          <p>
+            With his blend of technical skills, a keen eye for detail, and a
+            passion for excellence, Divyanshu is on a promising path toward
+            becoming a leader in the tech industry.
           </p>
           <h3>Skill & Intrest</h3>
           <div className="skillContainer">
-           <div>Html</div>
-           <div>CSS</div>
-           <div>JavaScript</div>
-           <div>React js</div>
-           <div>Node js</div>
-           <div>Express js</div>
-           <div>Mongo DB</div>
+            <div>Html</div>
+            <div>CSS</div>
+            <div>JavaScript</div>
+            <div>React js</div>
+            <div>Node js</div>
+            <div>Express js</div>
+            <div>Mongo DB</div>
+            <div>C++</div>
+            <div>Taildwind</div>
+            <div>Redux js</div>
           </div>
         </div>
       </div>
--- src/pages/Experience.jsx
+++ src/pages/Experience.jsx
@@ -11,22 +11,28 @@ const Experience = () => {
       </div>
       <div className="education_container">
         <div className="education_du_des">
-          <RiArrowRightDoubleFill style={{height:'5rem',width:'2.5rem',color:'#11cfe2ab'}}/>
+          <RiArrowRightDoubleFill
+            style={{ height: "5rem", width: "2.5rem", color: "#11cfe2ab" }}
+          />
           <div className="education_du_des_inner">
-            <h4>company name</h4>
-            <p>designation (duration)</p>
+            <h4>Solitaire Infosys</h4>
+            <p>Python Developer </p>
           </div>
         </div>
         <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa tempore
-          vel ratione expedita, maxime minima. Expedita incidunt dolore magni
-          porro illum laborum? Soluta, maxime cum. Rem quaerat harum facilis
-          nesciunt.
+          Developed a Snake game using Python and Tkinter, implementing %100 GUI
+          functionality. Created a dynamic game loop with real-time updates,
+          handling collision detection and score tracking. Utilized canvas for
+          rendering graphics and implemented keyboard controls for player
+          movement. Achieved smooth performance and user interaction with fewer
+          than 200 lines of code.
         </p>
       </div>
       <div className="education_container">
         <div className="education_du_des">
-          <RiArrowRightDoubleFill style={{height:'5rem',width:'2.5rem',color:'#11cfe2ab'}}/>
+          <RiArrowRightDoubleFill
+            style={{ height: "5rem", width: "2.5rem", color: "#11cfe2ab" }}
+          />
           <div className="education_du_des_inner">
             <h4>company name</h4>
             <p>designation (duration)</p>
