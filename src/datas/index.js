5152c8b11d4160e3e128b6340d3b3c6588f1476f
Divyanshu Shekhar <114979302+Divyanshu-Shekha@users.noreply.github.com>
2024-09-29 15:20:00 +0530

    new commit

--- src/datas/index.js
+++ src/datas/index.js
@@ -1,5 +1,6 @@
 import Tarzone from '../Assets/Tarxone.PNG';
 import PortFolio from '../Assets/portfolio.PNG'
+import Password_generator from '../Assets/Password_generator.PNG'
 const project = [{
     imgUrl : Tarzone,
     projectName : "Tarzone",
@@ -12,6 +13,13 @@ const project = [{
     projectDesc : "Dummy PortFolio",
     projectLink : "https://divyanshu-port.netlify.app"
 },
+{
+    imgUrl : Password_generator,
+    projectName : "Random Password Generator",
+    projectDesc : "To create any type of password including number and special charecter",
+    projectLink : "https://password-random-genereator.netlify.app/"
+},
+
 
 ]
 
