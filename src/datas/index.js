df1eb63ac42f95673e1a77238d7ba2d0e3dca9f7
Divyanshu Shekhar <divyanshushekhar20@gmail.com>
2024-11-30 10:57:56 +0530

    BookStore is added

--- src/datas/index.js
+++ src/datas/index.js
@@ -1,44 +1,40 @@
-import Tarzone from '../Assets/Tarxone.PNG';
-import BookStore from '../Assets/BookSTore.png';
-import PortFolio from '../Assets/portfolio.PNG'
-import Password_generator from '../Assets/Password_generator.PNG'
-import TODO from "../Assets/TODO.png"
+import Tarzone from "../Assets/Tarxone.PNG";
+import BookStore from "../Assets/BookSTore.png";
+import PortFolio from "../Assets/portfolio.PNG";
+import Password_generator from "../Assets/Password_generator.PNG";
+import TODO from "../Assets/TODO.png";
 const project = [
-    
-    
-    {
-        imgUrl : BookStore,
-        projectName : "All in One BookStore",
-        projectDesc : "A Book Store for All type of Readers ...",
-        projectLink : "https://allinonebookstore.netlify.app/"
-    },
-    
-    {
-    imgUrl : Tarzone,
-    projectName : "Tarzone",
-    projectDesc : "An ecommerce site...",
-    projectLink : "https://glistening-starburst-28a782.netlify.app/"
-},
-{
-    imgUrl : PortFolio,
-    projectName : "Portfolio",
-    projectDesc : "Dummy PortFolio",
-    projectLink : "https://divyanshu-port.netlify.app"
-},
-{
-    imgUrl : Password_generator,
-    projectName : "Random Password Generator",
-    projectDesc : "To create any type of password",
-    projectLink : "https://password-random-genereator.netlify.app/"
-},
-{
-    imgUrl : TODO,
-    projectName : "TODO",
-    projectDesc : "To manage your TODO'S",
-    projectLink : "https://todotaskcollector.netlify.app/"
-},
+  {
+    imgUrl: BookStore,
+    projectName: "All in One BookStore",
+    projectDesc: "A Book Store for All type of Readers ...",
+    projectLink: "https://allinonebookstore.netlify.app/",
+  },
 
-]
-
+  {
+    imgUrl: Tarzone,
+    projectName: "Tarzone",
+    projectDesc: "An ecommerce site...",
+    projectLink: "https://glistening-starburst-28a782.netlify.app/",
+  },
+  {
+    imgUrl: PortFolio,
+    projectName: "Portfolio",
+    projectDesc: "Dummy PortFolio",
+    projectLink: "https://divyanshu-port.netlify.app",
+  },
+  {
+    imgUrl: Password_generator,
+    projectName: "Random Password Generator",
+    projectDesc: "To create any type of password",
+    projectLink: "https://password-random-genereator.netlify.app/",
+  },
+  {
+    imgUrl: TODO,
+    projectName: "TODO",
+    projectDesc: "To manage your TODO'S",
+    projectLink: "https://todotaskcollector.netlify.app/",
+  },
+];
 
 export default project;
